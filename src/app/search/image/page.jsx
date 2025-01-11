28c722a07d11d22f594f42e6868c5aca3dae8fbe
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-01-11 18:21:59 +0900

    added search pages

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ImageSearch() {
+  return <div className="text-red-500">ImageSearch</div>;
+}
---
+++ src/app/search/web/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WebSearch() {
+  return <div>WebSearch</div>;
+}
