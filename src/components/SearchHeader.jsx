067fc39fdbafe2adcd712cd7ab8e62b721f5e2f8
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-01-11 18:55:56 +0900

    updated search header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { RxGear } from "react-icons/rx";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Searchbox from "./Searchbox";
+import { TbGridDots, TbGridGoldenratio } from "react-icons/tb";
 
 export default function SearchHeader() {
   return (
-    <div>SearchHeader</div>
-  )
+    <header className={`sticky top-0`}>
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"/"}>
+          <Image src={"/logo.png"} width={120} height={40} priority />
+        </Link>
+        <div className="flex-1">
+          <Searchbox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2">
+          <RxGear
+            className="bg-transparent hover:bg-gray-200 p-2 text-4xl
+          rounded-full cursor-pointer"
+          />
+          <TbGridDots className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
+        </div>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2">
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
 }
