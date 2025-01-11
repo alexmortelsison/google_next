9b31a4cff62f6bcfb60edf2faa8e7148521a260b
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-01-12 08:48:03 +0900

    fetch api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function WebSearch() {
-  return <div>WebSearch</div>;
+export default async function WebSearch({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+  return (
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
